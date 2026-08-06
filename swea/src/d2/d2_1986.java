52bca1993cd2a5500b65cf366878a9b2d728d2f0
김보경 <gumi060924@gmail.com>
2026-08-06 09:54:38 +0900

    [SWEA/D2] 1986. 지그재그 숫자

---
+++ swea/src/d2/d2_1986.java
@@ -0,0 +1,27 @@
+package d2;
+
+import java.util.Scanner;
+
+public class d2_1986 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			int ans = 0;
+			
+			for(int i=1; i<=N; i++) {
+				if(i%2 == 0) {
+					// Â¦¼ö
+					ans -= i;
+				} else {
+					// È¦¼ö
+					ans += i;
+				}
+			}
+			System.out.println("#" + test_case + " " + ans);
+		}
+	}
+}
