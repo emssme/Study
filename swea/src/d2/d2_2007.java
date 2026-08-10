d12a0af34273222f0464fc6698d555ad6e60a576
김보경 <bk75121@gmail.com>
2026-08-10 09:27:03 +0900

    [SWEA/D2] 2007. 패턴 마디의 길이

---
+++ swea/src/d2/d2_2007.java
@@ -0,0 +1,24 @@
+package d2;
+
+import java.util.Scanner;
+
+public class d2_2007 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			String str = sc.next();
+			int answer = 0;
+			
+			for(int i=1; i<10; i++) {
+				 if(str.substring(0, i).equals(str.substring(i, i+i))) {
+					 answer = i;
+					 break;
+				 }
+			}
+			System.out.println("#" + test_case + " " + answer);
+		}
+	}
+}
